d8e4d82f6d444ec5547a90ed1a94438542754ba8
Eshemeidomi Samuel Oghenevwede <eshemiedomisamuelb@gmail.com>
2024-03-25 03:06:54 +0100

    [fix]: updated addItem and sellItem schema validation

--- src/utils/joiSchema.ts
+++ src/utils/joiSchema.ts
@@ -1,11 +1,11 @@
 import Joi from "joi";
 
 export const addItemSchema = Joi.object().keys({
-    quantity: Joi.number().required(),
-    expiry: Joi.number().required(),
+    quantity: Joi.number().min(1).required(),
+    expiry: Joi.number().min(1).required(),
 });
 
 export const sellItemSchema = Joi.object().keys({
-    quantity: Joi.number().required(),
+    quantity: Joi.number().min(1).required(),
 });
 
