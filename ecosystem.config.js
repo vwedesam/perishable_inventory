84224e714451716e9039f1a181b39f8e7d8bf668
Eshemeidomi Samuel Oghenevwede <eshemiedomisamuelb@gmail.com>
2024-04-06 01:42:47 +0100

    [fix]: added pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+      name: "perishable_inventory_server",
+      script: 'npm run dev',
+      watch: './src'
+    }
+  ],
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
